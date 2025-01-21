adbe8b937f7ca7e4879a7bea476d4a0735abee3e
Omonov <algoritm1202@gmail.com>
2025-01-21 15:02:00 +0500

    scroll

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router } from "react-router-dom";
 import RouterComponent from "./router/router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Scroll from "./components/scroll/scroll";
 
 function App() {
   return (
     <div className="bg-[#f7f9fd] mx-auto min-h-screen">
       <Router>
         <RouterComponent />
+        <Scroll />
       </Router>
       <ToastContainer />
     </div>
